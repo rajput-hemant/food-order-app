961eab8df48ef19112045ddfe2eb6dbd1962c365
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-13 13:28:27 +0530

    created Header & HeaderCartButtons components
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import styles from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+	return (
+		<Fragment>
+			<header className={styles.header}>
+				<h1>ReactMeals</h1>
+				<HeaderCartButton />
+			</header>
+			<div className={styles["main-image"]}>
+				<img src={mealsImg} alt="A table full of food!" />
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;
