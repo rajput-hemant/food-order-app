d73a19792782de56c4880ffd00fc637c1a900448
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-11-14 10:54:51 +0530

    created CartItem component, upadted cartReducer logic to update the amount of item if it already exits in the cart
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,30 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
+import CartContext from "../../store/CartContext";
 
 const Cart = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = cartCtx.totalAmount.toFixed(2);
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemAddHandler = (item) => {};
+	const cartItemRemoveHandler = (id) => {};
+
 	const cartItems = (
 		<ul className={styles["cart-items"]}>
-			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-				<li>{item.name}</li>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onAdd={cartItemAddHandler.bind(null, item)}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+				/>
 			))}
 		</ul>
 	);
@@ -15,13 +34,13 @@ const Cart = (props) => {
 			{cartItems}
 			<div className={styles.total}>
 				<span>Total Amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={styles.actions}>
 				<button className={styles["button--alt"]} onClick={props.onClose}>
 					Close
 				</button>
-				<button className={styles.button}>Order</button>
+				{hasItems && <button className={styles.button}>Order</button>}
 			</div>
 		</Modal>
 	);
